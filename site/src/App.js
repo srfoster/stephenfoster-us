90646282a7b5b53a5e8faf02d85e2b970c4c8d80
Stephen Foster <stephen@thoughtstem.com>
2021-11-07 08:24:17 -0800

    more typesetting of a couple of works

--- site/src/App.js
+++ site/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect} from "react";
-import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch, useHistory } from "react-router-dom";
 import Fade from '@material-ui/core/Fade';
 import { ReactTerminal } from "react-terminal";
 import { TerminalContextProvider } from "react-terminal";
-import { Button, Card, CardActions, CardContent, CardHeader } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, CardHeader, Grid } from '@material-ui/core';
 import Pagination from '@mui/material/Pagination';
 import {text as codingForBabiesText} from './writings/coding-for-babies';
 import {text as politeBarbariansText} from './writings/polite-barbarians';
@@ -18,6 +18,10 @@ function App() {
 				<Router>
 					<Switch>
 						<Route path="/" exact component={Root} />
+						<Route path="/the-polite-barbarians" exact component={
+						   ()=> <FullDocument title="The Polite Barbarians" text={politeBarbariansText} />} />
+						<Route path="/coding-for-babies" exact component={
+						   ()=> <FullDocument title="Coding for Babies" text={codingForBabiesText} />} />
 					</Switch>
 				</Router>
 			</TerminalContextProvider>
@@ -59,27 +63,76 @@ const Root = (props) => {
 const Writings = () => {
   const [showing, setShowing] = useState()
   return showing ? showing : <>
-    <p>You found the writings accessible at your level.  Click a button below!</p>
-    <ul>
-		<li><Button variant="contained" color="secondary" onClick={(e)=>{setShowing(<ReadingWidget title="Coding for Babies" text={codingForBabiesText} />)}}>"Coding for Babies"</Button> - A satire on coding education and Silicon Valley culture.  I wrote this in late 2021. (I'll be typesetting it for the web over the next few days...)</li>
-		<li><Button variant="contained" color="secondary" onClick={(e)=>{setShowing(<ReadingWidget title="The Polite Barbarians" text={politeBarbariansText} />)}}>"Polite Barbarians"</Button> - A charming tale about Trojan horses.  I wrote this in early 2020. (I'll be typesetting it for the web over the next few days...)</li>
-	</ul>
-  </>
+	  <p>You found the writings accessible at your level.  Click a button below!</p>
+	  <Grid container spacing={24}>
+		  <Grid item md={3}>
+			  <WritingLink
+				  title="The Polite Barbarians"
+				  summary="A charming tale about Trojan horses.  I wrote this in early 2020."
+				  onClick={
+					  (e) => { setShowing(<ReadingWidget slug="the-polite-barbarians" title="The Polite Barbarians" text={politeBarbariansText} />) }
+				  } />
+		  </Grid>
+		  <Grid item md={3}>
+			  <WritingLink 
+			      title="Coding for Babies"
+				  summary="A satire on coding education and Silicon Valley culture.  I wrote this in late 2021."
+			      onClick={
+				      (e) => { setShowing(<ReadingWidget slug="coding-for-babies" title="Coding for Babies" text={codingForBabiesText} />) }
+			  } />
+		  </Grid>
+		  <Grid item md={3}>
+		  </Grid>
+	  </Grid>
+	</>
 }
 
+const WritingLink = (props) => {
+	return <>
+		<Card>
+			<CardContent>
+				<h3>{props.title}</h3>
+				<p>{props.summary}</p>
+				<a style={{cursor: "pointer"}} onClick={props.onClick}>[Read it!]</a>
+			</CardContent>
+		</Card>
+	</>
+}
 
+//		<li><Button variant="contained" color="secondary" onClick={(e)=>{setShowing(<ReadingWidget slug="the-polite-barbarians" title="The Polite Barbarians" text={politeBarbariansText} />)}}>"Polite Barbarians"</Button> - A charming tale about Trojan horses.  I wrote this in early 2020. (I'll be typesetting it for the web over the next few days...)</li>
 
-const ReadingWidget = (props) =>{
-	const [page, setPage] = useState(0) 
-	const pages = props.text.split("---PAGE---").map((c)=> <ReactMarkdown>{c}</ReactMarkdown>)
+const ReadingWidget = (props) => {
+	const [page, setPage] = useState(0)
+	const pages = props.text.split("---PAGE---").map((c) => <ReactMarkdown>{c}</ReactMarkdown>)
+	const history = useHistory()
 
 	return <>
 		<Fade in={true} timeout={1000}>
-			<Card style={{fontFamily: "Roboto", margin: 10}}>
+			<Card style={{ letterSpacing: "-.003em", lineHeight: 1.58, fontSize: 21, fontFamily: "medium-content-serif-font,Georgia,Cambria,\"Times New Roman\",Times,serif", margin: 10 }}>
 				<CardContent>
+					<a style={{ cursor: "pointer" }} onClick={() => history.push("/" + props.slug)}>[Share link]</a>
 					<h2>{props.title}</h2>
 					{pages[page]}
-					<Pagination onChange={(e,p)=>setPage(p - 1)} count={pages.length} color="primary" />
+					<Pagination onChange={(e, p) => setPage(p - 1)} count={pages.length} color="primary" />
+				</CardContent>
+			</Card>
+		</Fade>
+	</>
+}
+
+const FullDocument = (props) => {
+	const text = props.text.replace(/---PAGE---/g, "")
+	const history = useHistory()
+
+	return <>
+		<Fade in={true} timeout={1000}>
+			<Card style={{ letterSpacing: "-.003em", lineHeight: 1.58, fontSize: 21, fontFamily: "medium-content-serif-font,Georgia,Cambria,\"Times New Roman\",Times,serif", margin: 10 }}>
+				<CardContent>
+					<a style={{ cursor: "pointer" }} onClick={() => history.push("/")}>[Home]</a>
+					<h2>{props.title}</h2>
+					<ReactMarkdown>
+						{text}
+					</ReactMarkdown>
 				</CardContent>
 			</Card>
 		</Fade>
--- site/src/writings/polite-barbarians.js
+++ site/src/writings/polite-barbarians.js
@@ -8,8 +8,10 @@ The next morning, a giant wooden horse stood at the city gates, with a bow on to
 
 The next day, the sun rose to find a stack of envelopes resting on the scorched earth where the Trojan horse's ashes still lingered. The knights who cautiously investigated discovered thousands of handwritten apology notes, one for each citizen of the walled city. The gist was:
 
-> We, the polite barbarians, apologize for the crudeness of yesterday's onslaught. Your mayor is a most clever man.
-> — The Horde
+>  We, the polite barbarians, apologize for the crudeness of yesterday's onslaught. Your mayor is a most clever man.
+>  — The Horde
+
+---PAGE---
 
 These were brought directly to the mayor who discovered, upon close examination, that the backs of the letters were filled top to bottom with invisible ink. It was pro-barbarian propaganda that spoke of the barbarian king's love of Latin and Greek literature, and of the barbarian horde's comfortable lifestyle. Needless to say, the mayor had the letters burned.
 
@@ -17,11 +19,15 @@ The next day, the sun rose to find the mayor himself standing immobile outside h
 
 The archers were ready to torch it, but the mayor said the optics would be bad: the mayor melting in flames. Instead, after thorough inspection, he had it installed in the courtyard — a better message to the populace.
 
+---PAGE---
+
 For a week, there were no deliveries — but the mayor dared not open the gates, for the nights were alive with barbarian drums. As the food supplies dwindled and the people began to whisper in the streets, the mayor sent riders to ask for help from the king. None of them returned.
 
 Just as the grain silos were beginning to run low, the morning sun found carts of freshly baked bread at the city gates. Naturally, the mayor ordered the women and children to sample each loaf for poison before distributing the food to his knights. The next morning brought more bread, and no poison. And the next morning, the same. The scraps the knights left behind were given to the women and children.
 
 Just as the hottest days of summer began to arrive, the morning bread deliveries ceased. In their place were carts full of gold and jewels — which the mayor ordered be brought to his personal treasure rooms for thorough inspection. So it was for weeks, the grain silos growing empty and the mayor's treasure rooms growing full. The people began to whisper in the streets again, casting the occasional insult at the wax statue, now drooping in the summer heat. To cut down on the whispering, the mayor began passing out gold coins, rubies, and emeralds — even to the poorest of the peasants.
+
+---PAGE---
 
 The mayor promised great sums to any rider who would carry a message to the king. Many tried, but none returned.
 
@@ -29,11 +35,15 @@ By mid summer, the barbarians had set up a marketplace just outside of archery r
 
 Although it was forbidden to leave the city walls, the children of the poor knew how to sneak over the walls in the dead of night. Occasionally, they were caught sneaking back with a loaf in every pocket, and glasses of lemonade in each hand. The mayor had these unlucky ones executed publicly, beside his now-sagging wax statue.
 
+---PAGE---
+
 So it was for months, until finally, on the first day of winter, the king's cavalry arrived. Shining knights galloped across the grassy plane and laid waste to the thriving barbarian markets. The weary knights on the city walls cheered halfheartedly as the king's men set fire to the barbarian bakery and amphitheater. The peasants gathered in the courtyard and bowed their heads, listening mournfully to the sounds of the lemonade stands being razed, wood and glass obliterated by swords and hooves.
 
 The mayor rode into the courtyard on his best horse, wearing his best jewels. The gates were opened to receive the king's cavalry. To everyone's surprise, the king himself led — wearing his traditional helmet and breastplate of gold. The peasants dropped to their knees and laid down their own gold and jewels, knowing these would be taxed away soon anyway.
 
 The king and mayor dismounted and shook hands. The king, removing his helmet, turned out to be not the king everyone expected — but a rugged man wearing barbarian warpaint. “I am the barbarian king!” he announced. “And this is my horse, named Trojan, for I am a connoisseur of Greek literature.”
+
+---PAGE---
 
 The horse’s rump, on closer inspection, was branded with the word “Trojan,” which in retrospect should have been a dead giveaway, but if any of the guards watching from the walls during the sacking of the barbarian market had noticed, none of them had made the connection.
 
@@ -49,11 +59,15 @@ He grinned at the mayor and said, "You are a most clever man."
 
 “What was in the bread and jewels?” demanded the mayor. “I had them thoroughly checked.”
 
-“An attempt to disrupt your local economy, destabilize your currency, undermine your political power, and incite a popular uprising,” said the barbarian king.
+---PAGE---
+
+“Our hope was to destabilize your currency, undermine your political power, and incite a popular uprising,” said the barbarian king.
 
 “Well it didn’t work,” said the mayor, smugly.
 
 To the peasants, the barbarian king announced, “I’ll make you a deal. Keep your jewels! Tonight we feast! Drinks will be discounted! Or…” He paused. Weary peasants and knights leaned forward, holding their breath. “My horde and I will leave in peace — leaving your jewels, your mayor, your tax laws, and your very, very civilized, non-barbaric economic system intact.”
+
+---PAGE---
 
 The peasants looked uneasily at each other. The mayor folded his arms, standing tall — in contrast with his stooping wax statue. He smirked.
 
