df227f67d09ddb1544f9b30444733dbd96ef14e7
Stephen Foster <stephen@thoughtstem.com>
2022-10-18 17:52:42 -0700

    refactoring

---
+++ site/src/writings/main/2022/10/18/index.js
@@ -0,0 +1,11 @@
+import { FancyReactMarkdown, Score } from "../../../../../components/index"
+import { WritingIds, WritingLinkCard } from "../../../../../components/WritingLinkCards"
+
+
+export const text =
+[
+ <FancyReactMarkdown>{`# October 18th, 2022 - Monday
+   
+Fixed the weird styling on links and the rest of the refactoring items from Oct 14.  Small victories, yay!
+`}</FancyReactMarkdown>
+  ]
