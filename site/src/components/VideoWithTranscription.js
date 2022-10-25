8e5ce04821ea8c4d30f8d4d7ff1156dde6b28c4c
Stephen Foster <stephen@thoughtstem.com>
2022-10-24 18:12:21 -0700

    non video option for videowithtranscript

--- site/src/components/VideoWithTranscription.js
+++ site/src/components/VideoWithTranscription.js
@@ -1,34 +1,41 @@
 
 import { useState, useEffect, useMemo, createRef } from "react";
-import { Card, CardContent, Grid } from '@mui/material';
+import { Card, CardContent, Grid, Button } from '@mui/material';
 import ReactPlayer from 'react-player/lazy';
 
-let VideoWithTranscription = ({video,transcription}) => {
+let VideoWithTranscription = ({video,transcription, hideVideo}) => {
   let [current, setCurrent] = useState(0)
+  let [playing, setPlaying] = useState(false)
 
   let player = createRef()
 
   return <Card sx={{ display: { xs: "none", md: "block" } }}>
         <CardContent>
-          <ReactPlayer
-            ref={(p)=>player=p} width="100%"
-            url={video}
-            controls={true}
-            progressInterval={ 10 }
-            onProgress={(p) => { 
+      {hideVideo && <Button onClick={() => { setPlaying((p)=>!p) }}>Play</Button>}
+      
+      <ReactPlayer
+        playing={ playing}
+        ref={(p) => {
+          player = p;
+          if(hideVideo && p) p.wrapper.style.display = "none"
+        }} width="100%"
+        url={video}
+        controls={true}
+        progressInterval={10}
+        onProgress={(p) => {
 
-              //Simple but super in-efficient, will not work for long transcriptions...
-              //TODO: Rework this
-              for (let i in transcription.results.items) {
-                let item = transcription.results.items[i]
-                let end = item.end_time
-                let start = item.start_time
-                if (p.playedSeconds > start && p.playedSeconds <= end)
-                  setCurrent(i)
-              }
-            }}
-            onEnded={() => {  }}
-          />
+          //Simple but super in-efficient, will not work for long transcriptions...
+          //TODO: Rework this
+          for (let i in transcription.results.items) {
+            let item = transcription.results.items[i]
+            let end = item.end_time
+            let start = item.start_time
+            if (p.playedSeconds > start && p.playedSeconds <= end)
+              setCurrent(i)
+          }
+        }}
+        onEnded={() => { }}
+      />
           {transcription.results.items.map((item,i) => {
             let content = item.alternatives[0].content
             return <span
